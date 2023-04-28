049729d485cf2ad214547fbd0014b9e11f1c4e2a
ahmadbader <ahmadbader245@gmail.com>
2023-04-28 21:04:59 +0300

    add new routes

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import {dirname} from 'path'
+import { fileURLToPath } from 'url';
+
 import router from './routes/user_routes.js';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs')
 
 app.get('/', router);
+app.get('/dashboard', router);
+app.get('/logout', router);
 app.post('/login', router);
-app.post('/signup', router);
+app.get('/signup', router);
+app.get('/create', router);
 
 app.listen(3000, () => {
     console.log("Server is running")
