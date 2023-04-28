7222f10fd8fa1d851c1538f52d1f3a639979090b
ahmadbader <ahmadbader245@gmail.com>
2023-04-28 21:06:13 +0300

    add the new routes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,21 +1,24 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import UserOperations from '../controllers/controllers.js';
-
+import auth from '../middlerware/auth.js'
 
 
 const router = express.Router();
 router.use(bodyParser.urlencoded({extended: true}))
+router.use(express.json());
 
 /**
  * creating a new user controller
  */
 const user = new UserOperations("User Created!");
 
-
-router.get('/', user.dashboard);
+router.get('/', user.index);
+router.get('/dashboard', auth, user.dashboard);
+router.get('/logout', user.logout);
 router.post('/login', user.login);
-router.post('/signup', user.signup);
+router.get('/signup', user.signup);
+router.post('/create', user.create);
 
 
 export default router;
