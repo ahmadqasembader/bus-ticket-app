2db0109a393ecd7aceb5653e1ccf2b98d1a03bfe
ahmadbader <ahmadbader245@gmail.com>
2023-05-01 18:23:54 +0300

    edit on the ejs files

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -15,41 +15,49 @@ class UserOperations {
         let flag = false;
         //redirecting to the dashboard if the user didn't logout
         if(req.headers.cookie){
+
+            /// Getting the cookie
             let access_token = req.headers.cookie
             access_token = access_token.split("access_token=")
             access_token = access_token[1];
+            
+            /// Verifying the jwt token
             jwt.verify(access_token, config.TOKEN_KEY, (err, user) => {
                 if(err)
                     return err;
-                res.redirect('dashboard')
+                user = User.findOne(user.email);
+                console.log("Index: " + user);
+                res.redirect('/dashboard');
             });
         }else{
             //otherwise redirect to the login page
+            console.log("Login redirecting");
             res.render('login', {flag})
         }
     }
 
     dashboard(req, res) 
     {
-        const {user} = req.body.user
-        res.render('dashboard', {user})//send the data to the ejs file
+        const {email, name} = req.body
+        console.log("Dashboard User: " + email, name);
+        res.render('dashboard', {email, name})//send the data to the ejs file
     }
+
 
     async login(req, res) {
         /// Fetching user info
         let { email, password } = req.body;
 
         let flag = false;
-        const user = await User.findOne({ email })
+        const user = await User.findOne({ email });
         if (user && (await bcrypt.compare(password, user.passwordHashed)))
         {
             /// creating a jwt token for the login 
             /// and store it in the cookies
             const token = jwt.sign({ email, password }, config.TOKEN_KEY, { expiresIn: "24h" });
             res.cookie("access_token", token, { httpOnly: true });
-
-            console.log(req.body);
-            res.redirect('/')
+            res.redirect("/dashboard");
+            // res.render('dashboard', {user});
         }else
         {
             flag = true;
@@ -63,12 +71,11 @@ class UserOperations {
         res.render('signup')
     }
 
-    async create(req, res) {
+    create(req, res) {
         
         let { name, email, password, phoneNumber } = req.body;
-
-        const passwordHashed = await bcrypt.hash(password, 10);
-
+        
+        const passwordHashed = bcrypt.hash(password, 10);
         try {
             /**
              * 1. 
@@ -78,14 +85,12 @@ class UserOperations {
 
             const user = new User({ name, email, passwordHashed, phoneNumber });
 
-
             /**
              * 2.
              *      Generate a jwt token for the user
              *      and store it in the cookies
              */
 
-            //Generate JWT token for each created user
             const token = jwt.sign({ user }, config.TOKEN_KEY, { expiresIn: "24h" })
             user.token = token;
             res.cookie("access_token", token, { httpOnly: true })
@@ -97,7 +102,8 @@ class UserOperations {
              *      and redirect to the homepage
              */
             user.save();
-            res.redirect('/');
+            console.log("user saved!!");
+            res.render('dashboard', {user});
 
         } catch (error) {
             res.json(error);
